6b0e7891b3a7b4d2ec08865048a56ca56a02fe36
Pedro <pedroberorato@gmail.com>
2020-09-23 20:20:56 -0300

    Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,10 @@ import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
-    <Switch>
-        <Route path="/" component={Dashboard} />
-    </Switch>
-)
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
 
 export default Routes;
